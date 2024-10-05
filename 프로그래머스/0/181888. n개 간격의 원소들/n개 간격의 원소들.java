2657cb02bfd2d2ceb731f43efb88d72cb1f309da
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-10-05 15:35:28 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.05 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        
+        int size = (num_list.length +n -1) /n;
+        
+        int[] answer = new int[size];
+        int index = 0;
+        
+        for(int i=0; i<num_list.length; i+=n){
+            answer[index++] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
