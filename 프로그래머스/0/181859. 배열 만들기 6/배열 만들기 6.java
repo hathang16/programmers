22d5b50212b3462232be651595205f0b8f88767c
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-10-11 21:41:38 +0900

    [level 0] Title: 배열 만들기 6, Time: 41.76 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i=0;
+        int[] e = {-1};
+        
+        while (i< arr.length){
+            if (stk.isEmpty()){
+                stk.add(arr[i]);
+            } else if (stk.get(stk.size()-1) == arr[i]){
+                stk.remove(stk.size()-1);
+            } else {
+                stk.add(arr[i]);
+            }
+            i++;
+        }
+        
+        if(stk.isEmpty()){
+            return e;
+        }
+        
+        int[] answer = new int[stk.size()];
+        
+        for (int j=0; j<stk.size(); j++){
+            answer[j] = stk.get(j);
+        }
+        
+        return answer;
+    }
+}
