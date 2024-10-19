6d63b89ae0b5d519c48d479c92872cd8f7891a0c
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-10-19 16:11:18 +0900

    [level 0] Title: 최빈값 구하기, Time: 1.15 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        Arrays.sort(array);
+        int m = array[array.length-1];
+        int[] c = new int[m+1];
+        
+        for (int i = 0; i<array.length; i++){
+                c[array[i]] ++;
+        }
+        
+        int top = c[0];
+        
+        for (int i=1; i<c.length; i++){
+            if( top < c[i] ) {
+                top = c[i];
+                answer =i;
+            } else if (top == c[i]) {
+                answer = -1;
+            }
+        }
+        return answer;
+    }
+}
