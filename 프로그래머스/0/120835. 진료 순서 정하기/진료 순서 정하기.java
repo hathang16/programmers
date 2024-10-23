ed258e07094319b3455b184714b5611b65c84124
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-10-23 15:53:53 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        
+        for(int i=0; i<emergency.length; i++){
+            int c = 0;
+            for (int j=0; j<emergency.length; j++){
+                if (emergency[i] < emergency[j]){
+                    c++;
+                }
+            }
+            answer [i] = c +1;
+        }
+        
+        return answer;
+    }
+}
