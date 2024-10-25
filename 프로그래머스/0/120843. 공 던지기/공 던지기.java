9d1259c669d7f8521deea11e34e4b7f13f892972
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-10-25 14:35:18 +0900

    [level 0] Title: 공 던지기, Time: 0.08 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        for (int i=1; i <k; i++){
+            answer += 2;
+            answer %= numbers.length;
+        }
+        return numbers[answer];
+    }
+}
