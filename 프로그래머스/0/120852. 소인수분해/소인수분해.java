3ef3e60a217a5548dc423da58d65fae5309e5ee5
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-10-26 16:39:57 +0900

    [level 0] Title: 소인수분해, Time: 0.16 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int n) {
+        
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i=2; i<= n; i++){
+            while (n % i == 0){
+                if (!answer.contains(i)){
+                    answer.add(i);
+                }
+                n = n/i;
+            }
+        }
+        return answer;
+    }
+}
