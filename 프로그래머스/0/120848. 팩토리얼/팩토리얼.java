916e307fbefe79f261572ce7db3082bd8154d452
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-10-26 15:48:42 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int i = 1;
+
+        while (answer <=n) {
+            i++;
+            answer *= i;
+        }
+        
+        return i-1;
+    }
+}
