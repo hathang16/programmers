f086d9008960223238ec85b5dd2b38e8b5c23b77
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-11-04 22:58:47 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.06 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 1;
+        int max = 1;
+        
+        for (int i=1; i<=a; i++){
+            if (a % i == 0 && b % i ==0){
+                max = i;
+            }
+        }
+        
+        b /= max;
+        
+        while (b % 2 == 0) {
+            b /= 2;
+        }
+        while (b % 5 == 0) {
+            b /= 5;
+        }
+        
+        if (b != 1){
+            answer =2;
+        }
+        
+        return answer;
+    }
+}
