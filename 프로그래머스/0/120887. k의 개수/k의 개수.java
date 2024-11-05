195b1b729e40e503541b0548e766fbf027f2f687
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-11-05 23:28:10 +0900

    [level 0] Title: k의 개수, Time: 26.20 ms, Memory: 89.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        String target = String.valueOf(k);
+        
+        for (int num = i; num <= j; num++) {
+            String numStr = String.valueOf(num);
+            for (int l = 0; l < numStr.length(); l++) {
+                if (numStr.charAt(l) == target.charAt(0)) {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
