57b8cb786d7dae83e2237bc1cd05b96d26b39a65
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-11-08 22:58:22 +0900

    [level 1] Title: 추억 점수, Time: 2.29 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        HashMap<String, Integer> yearningMap = new HashMap<>();
+
+        for (int i=0; i<name.length; i++) {
+            yearningMap.put(name[i],yearning[i]);
+        }
+        
+        int[] answer = new int[photo.length];
+        
+        for (int i=0; i<photo.length; i++){
+            int s = 0;
+            
+            for (int j=0; j<photo[i].length; j++){
+                s += yearningMap.getOrDefault(photo[i][j], 0);
+            }
+            answer[i] =s;
+        }
+        
+        return answer;
+    }
+}
