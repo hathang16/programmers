142fa8fea53c8cdc0fe624403a1b9aa16c846e86
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-11-09 22:42:27 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        int n =0;
+        
+        for (int i=0; i<numbers.length; i++){
+            n +=numbers[i];
+        }
+        
+        return answer-n;
+    }
+}
