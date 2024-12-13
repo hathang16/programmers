a39d0396065e1428b9ff73ba84788de93fe3aba7
hathang16 <144975170+hathang16@users.noreply.github.com>
2024-12-13 13:53:51 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.12 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        
+        for (int i=left; i <= right; i++) {
+            if (isSquare(i)) {
+                answer -= i;
+            } else {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+    
+    private boolean isSquare(int num) {
+        int sqrt = (int) Math.sqrt(num);
+        return sqrt * sqrt == num;
+    }
+}
