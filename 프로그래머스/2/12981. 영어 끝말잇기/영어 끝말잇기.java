5f90a1da9e9f817e1d62ea6cc02080b45e8990ea
hathang16 <144975170+hathang16@users.noreply.github.com>
2025-01-30 22:03:29 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        HashSet<String> hash = new HashSet<>();
+        hash.add(words[0]); 
+        
+        for (int i = 1; i < words.length; i++) {
+            String a = words[i-1];
+            String b = words[i];
+            
+            if (a.charAt(a.length() - 1) != b.charAt(0) || hash.contains(b)) {
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                return answer;
+            }
+            
+            hash.add(b);
+        }
+
+        return answer;
+    }
+}
+
+//배열
+// class Solution {
+//     public int[] solution(int n, String[] words) {
+//         int[] answer = new int[2];
+//         String[] usedWords = new String[words.length];
+//         usedWords[0] = words[0];
+
+//         for (int i = 1; i < words.length; i++) {
+//             String prev = words[i - 1];
+//             String current = words[i];
+
+//             if (prev.charAt(prev.length() - 1) != current.charAt(0)) {
+//                 answer[0] = (i % n) + 1;
+//                 answer[1] = (i / n) + 1;
+//                 return answer;
+//             }
+
+//             for (int j = 0; j < i; j++) {
+//                 if (usedWords[j].equals(current)) {
+//                     answer[0] = (i % n) + 1;
+//                     answer[1] = (i / n) + 1;
+//                     return answer;
+//                 }
+//             }
+
+//             usedWords[i] = current;
+//         }
+
+//         return answer;
+//     }
+// }
