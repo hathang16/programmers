9d20c1b4a0ce6d7704979740dca527994e9ba441
hathang16 <hathang16@naver.com>
2025-03-29 21:32:01 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -2,10 +2,10 @@ class Solution {
     public double solution(int[] arr) {
         double answer = 0;
         
-        for (int i=0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++){
             answer += arr[i];
         }
         
-        return answer/arr.length;
+        return answer / arr.length;
     }
 }
