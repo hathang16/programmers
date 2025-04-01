6df7190dd94d04e4e946f52188952aea49068c51
hathang16 <hathang16@naver.com>
2025-04-01 21:40:36 +0900

    [level 1] Title: 옹알이 (2), Time: 22.04 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,8 +3,9 @@ class Solution {
         int answer = 0;
         String[] str = {"aya", "ye", "woo", "ma"};
         String[] repeatStr = {"ayaaya", "yeye", "woowoo", "mama"};
-        for(int i=0; i<babbling.length; i++){
-            for(int j=0; j<str.length; j++){
+        
+        for(int i = 0; i < babbling.length; i++){
+            for(int j = 0; j < str.length; j++){
                 babbling[i] = babbling[i].replaceAll(repeatStr[j], "1").replaceAll(str[j], " ");
             }
             
