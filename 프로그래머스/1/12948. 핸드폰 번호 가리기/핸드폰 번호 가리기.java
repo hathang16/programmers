e53268fdbe57f9f389ab9880f5a3371ea9a2c5eb
hathang16 <hathang16@naver.com>
2025-04-03 21:56:35 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 16.85 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(String phone_number) {
         String answer = "";
-        for (int i=0; i<phone_number.length(); i++){
-            if ( i< phone_number.length() -4) {
+        for (int i = 0; i < phone_number.length(); i++){
+            if ( i < phone_number.length() - 4) {
                 answer += "*";
             } else {
                 answer += phone_number.charAt(i);
