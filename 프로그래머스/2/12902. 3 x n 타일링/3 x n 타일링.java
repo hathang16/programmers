2b70036376a5b454f2d3d0303b002143f8551e6b
hathang16 <hathang16@naver.com>
2025-04-03 21:58:48 +0900

    [level 2] Title: 3 x n 타일링, Time: 1.11 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -2,16 +2,16 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        int[] dp = new int[n+1];
+        int[] dp = new int[n + 1];
         dp[1] = 2;
         dp[2] = 3;
         for(int i = 3; i <= n; i++){
-            if(i%2 == 0){
+            if(i % 2 == 0){
                 //짝수
-                dp[i] = dp[i-1] % 1000000007 + dp[i-2] % 1000000007;
+                dp[i] = dp[i - 1] % 1000000007 + dp[i - 2] % 1000000007;
             } else {
                 // 홀수
-                dp[i] = dp[i-1] * 2 % 1000000007 + dp[i-2] % 1000000007;
+                dp[i] = dp[i - 1] * 2 % 1000000007 + dp[i - 2] % 1000000007;
             }
             dp[i] = dp[i] % 1000000007;
         }
