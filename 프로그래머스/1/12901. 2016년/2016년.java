ff8a305b6d641984262786038d6839cd04cac001
hathang16 <hathang16@naver.com>
2025-04-09 22:57:10 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -3,11 +3,11 @@ class Solution {
         int[] Month = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         String[] Week = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
         
-        for (int i=1; i<a; i++){
+        for (int i = 1; i < a; i++){
             b += Month[i];
         }
         
-        String answer = Week[(b+4)%7];
+        String answer = Week[(b + 4) % 7];
         return answer;
     }
 }
