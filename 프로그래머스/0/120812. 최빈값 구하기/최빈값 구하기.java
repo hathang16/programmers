2cd3519a36ea2702dc0ec8d32648ab33f894072e
hathang16 <hathang16@naver.com>
2025-04-20 21:37:44 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.57 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -5,19 +5,19 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(array);
-        int m = array[array.length-1];
-        int[] c = new int[m+1];
+        int m = array[array.length - 1];
+        int[] c = new int[m + 1];
         
-        for (int i = 0; i<array.length; i++){
+        for (int i = 0; i < array.length; i++){
                 c[array[i]] ++;
         }
         
         int top = c[0];
         
-        for (int i=1; i<c.length; i++){
+        for (int i = 1; i < c.length; i++){
             if( top < c[i] ) {
                 top = c[i];
-                answer =i;
+                answer = i;
             } else if (top == c[i]) {
                 answer = -1;
             }
