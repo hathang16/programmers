f6ec275cb6597277b51659e4331f1a2619c78c97
hathang16 <hathang16@naver.com>
2025-04-21 23:41:26 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 91.6 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/README.md
+++ 프로그래머스/0/120813. 짝수는 싫어요/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수는 싫어요 - 120813 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120813) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120813?language=java) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 0.02 ms
+메모리: 91.6 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 19일 16:16:29
+2025년 04월 21일 23:41:23
 
 ### 문제 설명
 
--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[(n+1)/2];
+        int[] answer = new int[(n + 1) / 2];
         
-        for(int i=1; i< n+1; i++){
+        for(int i = 1; i < n + 1; i++){
             if(i % 2 == 1){
-                answer [i/2] = i;
+                answer [i / 2] = i;
             }
         }
         return answer;
