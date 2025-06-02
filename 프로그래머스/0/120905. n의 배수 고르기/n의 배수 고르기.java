efa642ea961770562b34bdc7db014941c8bca0d5
hathang16 <hathang16@naver.com>
2025-06-02 23:02:01 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 70 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] solution(int n, int[] numlist) {
         int a = 0;
         
-        for (int i=0; i<numlist.length; i++){
+        for (int i = 0; i < numlist.length; i++){
             if (numlist[i] % n == 0){
                 a++;
             }
@@ -11,7 +11,7 @@ class Solution {
         int[] answer = new int[a];
         int index =0; 
         
-        for (int i=0; i<numlist.length; i++){
+        for (int i = 0; i < numlist.length; i++){
             if (numlist[i] % n == 0){
                 answer[index] = numlist[i];
                 index++;
