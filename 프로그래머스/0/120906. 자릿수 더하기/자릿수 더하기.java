6fb9d6f1255c7130e102894ee423f1c07562f4cf
hathang16 <hathang16@naver.com>
2025-06-14 23:25:34 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        while (n>0){
+        while (n > 0){
             answer += n % 10;
             n = n / 10;
         }
