2c4a6b32f9a87f6254566a3503f9e3ca4e94eb25
hathang16 <hathang16@naver.com>
2025-06-15 23:32:56 +0900

    [level 0] Title: 숫자 찾기, Time: 0.16 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,16 +1,17 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
+        int answer = -1;
         
         String s = Integer.toString(num);
         String[] str = s.split("");
         
-        for (int i=0; i<str.length; i++){
+        for (int i = 0; i < str.length; i++){
             if (str[i].equals(Integer.toString(k))){
-                return i+1;
-            } 
+                answer = i + 1;
+                break;
+            }
         }
         
-        return -1;
+        return answer;
     }
 }
