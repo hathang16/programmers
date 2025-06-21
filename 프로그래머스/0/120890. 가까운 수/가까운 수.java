f945cfc0ab1f3817d93bfbe23b490cb2cbc75ce9
hathang16 <hathang16@naver.com>
2025-06-21 16:59:40 +0900

    [level 0] Title: 가까운 수, Time: 0.54 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -5,8 +5,8 @@ class Solution {
         int answer = 0;
         Arrays.sort(array);
         
-        for(int i=0; i<array.length; i++){
-            if (i==0 || Math.abs(array[i]-n) < Math.abs(answer-n)){
+        for(int i = 0; i < array.length; i++){
+            if (i == 0 || Math.abs(array[i] - n) < Math.abs(answer - n)){
                 answer = array[i];
                 if(answer == n){
                     break;
