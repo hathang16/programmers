106b940fc7dd9af52e6a13cfec22ddf970bba8ed
hathang16 <hathang16@naver.com>
2025-07-04 13:23:10 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -2,9 +2,10 @@ class Solution {
     public int[] solution(String[] strlist) {
         int[] answer = new int[strlist.length];
         
-        for (int i=0; i<answer.length; i++){
+        for (int i = 0; i < answer.length; i++){
             answer[i] = strlist[i].length();
         }
+        
         return answer;
     }
 }
