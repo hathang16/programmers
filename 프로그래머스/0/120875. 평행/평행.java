bbf3c61f5a772659aceea8186439ffeb6ecf4e61
hathang16 <hathang16@naver.com>
2025-07-16 23:04:15 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -7,14 +7,14 @@ class Solution {
         double slope2 = (double)(dots[3][1] - dots[2][1]) / (dots[3][0] - dots[2][0]);
 
         if (slope1 == slope2) {
-            answer =1;
+            answer = 1;
         }
 
         slope1 = (double)(dots[2][1] - dots[0][1]) / (dots[2][0] - dots[0][0]);
         slope2 = (double)(dots[3][1] - dots[1][1]) / (dots[3][0] - dots[1][0]);
 
         if (slope1 == slope2) {
-            answer =  1;
+            answer = 1;
         }
 
         slope1 = (double)(dots[1][1] - dots[0][1]) / (dots[1][0] - dots[0][0]);
