497e0ab454baf88699691e29fe9d890539145931
hathang16 <hathang16@naver.com>
2025-07-21 12:40:54 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.04 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -3,8 +3,8 @@ class Solution {
         int answer = 1;
         int max = 1;
         
-        for (int i=1; i<=a; i++){
-            if (a % i == 0 && b % i ==0){
+        for (int i = 1; i <= a; i++){
+            if (a % i == 0 && b % i == 0){
                 max = i;
             }
         }
@@ -19,7 +19,7 @@ class Solution {
         }
         
         if (b != 1){
-            answer =2;
+            answer = 2;
         }
         
         return answer;
